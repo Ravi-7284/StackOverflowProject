ba97451a2725fbeba55b44895a2b56f1311530b9
Ravikeeran <Ravikeeran@DESKTOP-DNLP9V3>
2022-04-26 15:29:28 +0530

    Add project files.

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        // GET: Employees
+        [Authorize]
+        public IEnumerable <Employee> Get()
+        {
+            using (EmployeeDBEntities Entities = new EmployeeDBEntities())
+            {
+                return Entities.Employees.ToList();
+            }
+        }
+    }
+}
---
+++ StackOverflowProject.Repositories/VotesRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IVotesRepository
+    {
+        void UpdateVote(int aid, int uid, int value);
+
+    }
+
+    public class VotesRepository : IVotesRepository
+    {
+        StackOverflowDatabaseDbContext db;
+
+        public VotesRepository()
+        {
+            db = new StackOverflowDatabaseDbContext();
+        }
+
+        public void UpdateVote(int aid, int uid, int value)
+        {
+            int UpdateValue;
+            if (value > 0)
+            {
+                UpdateValue = 1;
+            }
+            else if (value < 0)
+            {
+                UpdateValue = -1;
+            }
+            else
+            {
+                UpdateValue = 0;
+            }
+
+            Vote v = db.Votes.Where(temp => temp.AnswerID == aid && temp.UserID == uid).FirstOrDefault();
+
+            if (v != null)
+            {
+                v.VoteValue = UpdateValue;
+            }
+            else
+            {
+                Vote newvote = new Vote()
+                {
+                    AnswerID = aid,
+                    UserID = uid,
+                    VoteValue = UpdateValue
+                };
+                db.Votes.Add(newvote);
+            }
+            db.SaveChanges();
+        }
+    }
+}
